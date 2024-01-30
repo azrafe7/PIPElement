77ab8626695e34619bbacc95c1ec73e4e7b30352
Giuseppe Di Mauro <azrafe7@gmail.com>
2024-01-30 03:02:08 +0100

    [v0.2.1]

--- contentScript.js
+++ contentScript.js
@@ -1,13 +1,13 @@
 "use strict";
 
 (async () => {
-  const DEBUG = true;
+  const DEBUG = false;
   let debug = {
     log: DEBUG ? console.log.bind(console) : () => {} // log or NO_OP
   }
 
   let manifest = chrome.runtime.getManifest();
-  console.log(manifest.name + " v" + manifest.version);
+  console.log(manifest.name + " v" + manifest.version + (DEBUG ? '[DEBUG]' : ''));
 
   const HIGHLIGHT_RED = "rgba(250, 70, 60, 0.5)";
   const HIGHLIGHT_GREEN = "rgba(17, 193, 12, 0.5)";
@@ -245,7 +245,7 @@
           video.style.setProperty(POINTER_EVENTS, 'all', 'important');
         }
       }
-      debug.log('fixedVideosMap:', fixedVideosMap)
+      debug.log(`fixedVideosMap (${fixedVideosMap}):`, fixedVideosMap)
       
       let innermostTargetAtPoint = null; // first non-picker-iframe element
       
